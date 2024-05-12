a10e4807c979c204db2d5ca3e2c69368c72cc34b
Hugo Pereira <hugopereira.contato@gmail.com>
2024-05-12 20:32:17 -0300

    update site
    concluída sessão "Contact me"

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const homeSection = document.querySelector('.main-principal');
 const aboutSection = document.querySelector('.bg-about');
 const serviceSection = document.querySelector('.bg-services');
 const projectSection = document.querySelector('.project-title');
+const contactSection = document.querySelector('.bg-contact-me');
 
 // Define a margem superior para ativar a classe scrolled-project
 const projectMargin = 200; // Ajuste conforme necessário
@@ -14,36 +15,25 @@ window.addEventListener('scroll', function() {
     const servicePosition = serviceSection.offsetTop;
     const projectPosition = projectSection.offsetTop;
     const scrolledPosition = projectPosition - projectMargin;
+    const contactPosition = contactSection.offsetTop;
 
     if (scrollPosition < aboutPosition) {
-        header.classList.remove('scrolled-about');
-        header.classList.remove('scrolled-service');
-        header.classList.remove('scrolled-project');
-        header.classList.remove('scrolled-project-margin');
+        header.classList.remove('scrolled-about', 'scrolled-service', 'scrolled-project', 'scrolled-project-margin', 'scrolled-contact');
         header.classList.add('scrolled-home');
     } else if (scrollPosition >= aboutPosition && scrollPosition < servicePosition) {
-        header.classList.remove('scrolled-home');
-        header.classList.remove('scrolled-service');
-        header.classList.remove('scrolled-project');
-        header.classList.remove('scrolled-project-margin');
+        header.classList.remove('scrolled-home', 'scrolled-service', 'scrolled-project', 'scrolled-project-margin', 'scrolled-contact');
         header.classList.add('scrolled-about');
     } else if (scrollPosition >= servicePosition && scrollPosition < scrolledPosition) {
-        header.classList.remove('scrolled-home');
-        header.classList.remove('scrolled-about');
-        header.classList.remove('scrolled-project');
-        header.classList.remove('scrolled-project-margin');
+        header.classList.remove('scrolled-home', 'scrolled-about', 'scrolled-project', 'scrolled-project-margin', 'scrolled-contact');
         header.classList.add('scrolled-service');
     } else if (scrollPosition >= scrolledPosition && scrollPosition < projectPosition) {
-        header.classList.remove('scrolled-home');
-        header.classList.remove('scrolled-about');
-        header.classList.remove('scrolled-service');
-        header.classList.remove('scrolled-project');
+        header.classList.remove('scrolled-home', 'scrolled-about', 'scrolled-service', 'scrolled-project', 'scrolled-contact');
         header.classList.add('scrolled-project-margin');
-    } else {
-        header.classList.remove('scrolled-home');
-        header.classList.remove('scrolled-about');
-        header.classList.remove('scrolled-service');
-        header.classList.remove('scrolled-service-margin');
+    } else if (scrollPosition >= projectPosition && scrollPosition < contactPosition) {
+        header.classList.remove('scrolled-home', 'scrolled-about', 'scrolled-service', 'scrolled-project', 'scrolled-contact');
         header.classList.add('scrolled-project');
+    } else if (scrollPosition >= contactPosition) {
+        header.classList.remove('scrolled-home', 'scrolled-about', 'scrolled-service', 'scrolled-project', 'scrolled-project-margin');
+        header.classList.add('scrolled-contact');
     }
 });
